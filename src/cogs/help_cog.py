6651d10639c893cb5f4f50d5a2b7858f2d1c71ca
Ramil Mavliutov <stirk2281337@gmail.com>
2024-02-15 01:32:21 +0500

    Bot randomly answers

--- src/cogs/help_cog.py
+++ src/cogs/help_cog.py
@@ -1,11 +1,14 @@
+import asyncio
+import random
+
 import discord
 from discord import app_commands
-from discord.ext import tasks
+from discord.ext import tasks, commands
 from discord.ext.commands import Bot
 
-from config import BOT_CHAT_ID
+from config import BOT_CHAT_ID, MAIN_CHAT_ID
 from src.cogs.base import BaseCog
-from src.utils.emoji_utils import get_random_formatted_emoji
+from src.utils.emoji_utils import get_random_formatted_emoji, get_random_sticker
 
 
 class Help(BaseCog):
@@ -19,7 +22,17 @@ class Help(BaseCog):
 
     @tasks.loop(hours=1)
     async def spam(self):
-        channel = self.bot.get_channel(BOT_CHAT_ID)  # main channel
+        channel = self.bot.get_channel(MAIN_CHAT_ID)  # main channel
         formatted_emoji = get_random_formatted_emoji(channel.guild)
         await channel.send(formatted_emoji)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.id in [self.bot.user.id]:
+            return
+
+        random_number = random.randint(0, 50)
+        if random_number == 1:
+            await asyncio.sleep(10)
+            await message.channel.send(stickers=[get_random_sticker(message.channel.guild)], reference=message)
 
